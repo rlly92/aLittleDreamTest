31690286e14f35f5c5b1febd4c905c69f38b0184
rlly92 <reubenlimliangyi@gmail.com>
2023-10-06 18:26:01 +0800

    Used AWS Amplify CLI command to install config files for cloud BE

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "aLittleDreamTest": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
