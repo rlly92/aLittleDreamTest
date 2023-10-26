19cc58ff5203ab5f3094814ca12f53649192b2d3
rlly92 <reubenlimliangyi@gmail.com>
2023-10-26 22:46:07 +0800

    auth built

--- src/App.js
+++ src/App.js
@@ -3,18 +3,22 @@ import logo from "./logo.png";
 import "./App.css";
 import InputTestForm from "./components/InputTestForm";
 import AdminSignIn from "./components/AdminSignIn";
+import { Authenticator } from "@aws-amplify/ui-react";
 
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>HELLO WORLD. AWS SUCKS.</p>
-          <InputTestForm />
-          <AdminSignIn />
-        </header>
-      </div>
+      <Authenticator>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>HELLO WORLD. AWS SUCKS.</p>
+            <InputTestForm />
+
+            <AdminSignIn />
+          </header>
+        </div>
+      </Authenticator>
     );
   }
 }
--- src/components/AdminSignIn.js
+++ src/components/AdminSignIn.js
@@ -3,16 +3,17 @@ import { Amplify } from "aws-amplify";
 import { withAuthenticator } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
 
-import awsExports from "./aws-exports";
+import awsExports from "../aws-exports";
 Amplify.configure(awsExports);
 
-const AdminSignIn = (signOut, user) => {
+const AdminSignIn = ({ signOut, user }) => {
+  console.log(user);
   return (
     <>
-      <h1>Hello {user.username}</h1>
+      <h1>Hello {user.attributes.email}</h1>
       <button onClick={signOut}>Sign out</button>
     </>
   );
 };
 
-export default withAuthenticator(AdminSignIn);
+export default withAuthenticator(AdminSignIn, { hideSignUp: true });
